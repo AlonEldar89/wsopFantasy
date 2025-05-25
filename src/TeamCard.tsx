1dcc81b6a1a4c50118e7ef287f2e07c721cf8180
PTK Web Developer <ptkweb@example.com>
2025-05-25 13:33:48 +0300

    fix

--- src/TeamCard.tsx
+++ src/TeamCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import teamData from '../mockTeam.json';
+import teamData from './mockTeam.json';
 
 const avatarColors = [
   'bg-blue-500', 'bg-green-500', 'bg-yellow-500', 'bg-pink-500', 'bg-purple-500', 'bg-red-500', 'bg-teal-500', 'bg-orange-500',
